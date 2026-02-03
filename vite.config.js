5cf9f0990c91c11724047d3bbdde78ee94c4b127
Illya Glukhanyuk <ig@bimhelp.me>
2026-02-03 07:50:01 +0000

    vite fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-XDirection Width 
-		Racking XDirection Axis Length + XDirection Offset To End
-		Racking XDirection Axis Length + 2 * XDirection Offset To End
-ZDirection Width
-		Racking Axis Width * Amount Of Blocks + Aisle Axis Width * (Amount Of Blocks - 1)
-		
-FOT
-left
-5214115370
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -16,4 +7,4 @@ export default defineConfig({
   build: {
     outDir: "dist", // Папка, куда Vite положит готовый сайт
   },
-})
+});
