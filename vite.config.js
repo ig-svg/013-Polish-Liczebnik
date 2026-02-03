0f2114ab2a27efba5553ac31953b72d3d5c83d05
Illya Glukhanyuk <ig@bimhelp.me>
2026-02-03 07:42:45 +0000

    vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+XDirection Width 
+		Racking XDirection Axis Length + XDirection Offset To End
+		Racking XDirection Axis Length + 2 * XDirection Offset To End
+ZDirection Width
+		Racking Axis Width * Amount Of Blocks + Aisle Axis Width * (Amount Of Blocks - 1)
+		
+FOT
+left
+5214115370
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Основная настройка для работы React через Vite
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Папка, куда Vite положит готовый сайт
+  },
+});
