6e0620997a6bac3b87acdc5f2ee26193004af0f4
Illya Glukhanyuk <ig@bimhelp.me>
2026-02-03 08:09:16 +0000

    vite fix build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Папка, куда Vite положит готовый сайт
+    outDir: "build", // Папка, куда Vite положит готовый сайт
   },
 })
