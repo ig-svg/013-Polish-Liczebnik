dbbc5a3977b5bf1df21d704fb71b8d33fd7be033
Illya Glukhanyuk <ig@bimhelp.me>
2026-02-03 08:27:54 +0000

    adding css

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css'; // Исправлено: импортируем styles.css, так как файл в src называется именно так
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <App />
